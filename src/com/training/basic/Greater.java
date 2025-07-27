9a0af2956505b1757909b2fb69d6e4493c20c485
krithika <krithikakrishnamoorthy17@gmail.com>
2025-07-27 02:19:21 -0400

    largest value

--- src/com/training/basic/Greater.java
+++ src/com/training/basic/Greater.java
@@ -1,29 +1,28 @@
 package com.training.basic;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Greater {
 
 	public static void main(String[] args) {
-  int[] greaterArray = new int[4];
-  greaterArray[0] = 107;
-  greaterArray[1] = 103;
-  greaterArray[2] = 106;
-  greaterArray[3] = 108;
-  
-for(int i=0;i<greaterArray.length;i++) {
-	if(greaterArray[i]<greaterArray[i+1]) 
-	{
-		
-System.out.println(greaterArray[i+1]);
+		int[] greaterArray = new int[4];
+		greaterArray[0] = 102;
+		greaterArray[1] = 140;
+		greaterArray[2] = 196;
+		greaterArray[3] = 111;
+		for (int i = 0; i < greaterArray.length; i++) {
+			int j = i + 1;
+			if (i == greaterArray.length - 1) {
+				j = i - 1;
+			}
+			if (greaterArray[i] > greaterArray[j]) {
+				greaterArray[j] = greaterArray[i];
+			}
 		}
+		System.out.println("Largest array :" + greaterArray[greaterArray.length - 1]);
 	}
-}
 
- // Scanner sc = new Scanner(System.in);
-  
+	// Scanner sc = new Scanner(System.in);
 
-  
 //sc.close();
-	}
-
+}
