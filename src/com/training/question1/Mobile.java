c8a53db3b7f45ae5a0807ea81bbd4f81b8f49a1b
krithika <krithikakrishnamoorthy17@gmail.com>
2025-07-30 11:13:26 -0400

    object assignmnets

---
+++ src/com/training/question1/Mobile.java
@@ -0,0 +1,28 @@
+package com.training.question1;
+
+public class Mobile {
+	
+public Mobile(String model, double price, String brand) {
+		super();
+		this.model = model;
+		this.price = price;
+		this.brand = brand;
+	}
+String model;
+double price;
+String brand;
+
+void getDetails() {
+	System.out.println("Model :"+model);
+	System.out.println("Price :"+price);
+	System.out.println("Brand :"+brand);
+}
+
+String[] showFeatures(String Model,double Price,String Brand) {
+	if(Model.equals("A5")){
+	 String[] features= new String[]{"8pixcel","goriella glass"};
+	 return features;
+	}
+	return null;
+}
+}
